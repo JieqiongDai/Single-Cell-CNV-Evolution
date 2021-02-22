bb6dea9fc7e3c2660ba881bc7052f55b119bd435
Jieqiong Dai <40248157+JieqiongDai@users.noreply.github.com>
2021-02-22 17:18:22 -0500

    Update cytoscape_patient.R

--- script/cytoscape_patient.R
+++ script/cytoscape_patient.R
@@ -1,10 +1,41 @@
-#load libary for cytoscape and dependent packages
-if(!"RCy3" %in% installed.packages()){
-  install.packages("BiocManager")
-  BiocManager::install("RCy3")
+# Check required packages
+# function for installing needed packages
+installpkg <- function(x){
+  r = getOption("repos")
+  r["CRAN"] = "http://cran.us.r-project.org"
+  options(repos = r)
+  if(x %in% rownames(installed.packages())==FALSE) {
+    if(x %in% rownames(available.packages())==FALSE) {
+      if (!requireNamespace("BiocManager", quietly = TRUE))
+        install.packages("BiocManager")
+      BiocManager::install(x,ask = FALSE)
+    } else {
+      install.packages(x)
+    }
+    paste(x, "package is installed...")
+  } else {
+    paste(x,"package already installed...")
+  }
 }
+
+# install necessary packages
+required_packages  <- c('RCy3','argparse')
+lapply(required_packages,installpkg)
+
+#load libary for cytoscape and dependent packages
 library(RCy3)
 cytoscapePing ()
+
+library(argparse)
+parser <- ArgumentParser()
+parser$add_argument('-s',
+                    '--style',
+                    nargs='+',
+                    required=TRUE,
+                    help="cytoscape style file")
+args <-parser$parse_args()
+
+style <- c(args$style)
 
 #create cytoscape network
 nodes <- data.frame(read.table("node_cyto.txt",sep="\t",header=T,row.names=NULL))
@@ -18,6 +49,8 @@ edges $ dist <- as.numeric(edges $ dist)
 createNetworkFromDataFrames(nodes,edges)
 lockNodeDimensions(TRUE)
 #switch styles
+if ("scCNV_group" %in% getVisualStyleNames() == "FALSE")
+importVisualStyles(style)
 setVisualStyle('scCNV_group')
 #set node size mapping
 setNodeSizeMapping('cell.number', c(min(nodes$cell.number),max(nodes$cell.number)), c(10,60),style.name = "scCNV_group")
